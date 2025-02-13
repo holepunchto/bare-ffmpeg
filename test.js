333f83aa7489650cc9bb64e13fc2712856b34d22
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-02-13 11:57:43 +0100

    Test decoding AVIF

--- test.js
+++ test.js
@@ -51,6 +51,56 @@ test('decode .heic', (t) => {
   io.destroy()
 })
 
+test('decode .avif', (t) => {
+  const image = require('./test/fixtures/image/sample.avif', {
+    with: { type: 'binary' }
+  })
+
+  const io = new ffmpeg.IOContext(image)
+  const format = new ffmpeg.FormatContext(io)
+
+  for (const stream of format.streams) {
+    const packet = new ffmpeg.Packet()
+    format.readFrame(packet)
+
+    const raw = new ffmpeg.Frame()
+    const rgba = new ffmpeg.Frame()
+
+    const decoder = stream.decoder()
+    decoder.sendPacket(packet)
+    packet.unref()
+
+    decoder.receiveFrame(raw)
+
+    const image = new ffmpeg.Image('RGBA', decoder.width, decoder.height)
+    image.fill(rgba)
+
+    const scaler = new ffmpeg.Scaler(
+      decoder.pixelFormat,
+      decoder.width,
+      decoder.height,
+      image.pixelFormat,
+      image.width,
+      image.height
+    )
+
+    scaler.scale(raw, rgba)
+    scaler.destroy()
+
+    packet.destroy()
+    raw.destroy()
+    rgba.destroy()
+    decoder.destroy()
+
+    t.comment('width', image.width)
+    t.comment('height', image.height)
+    t.comment('data', image.data)
+  }
+
+  format.destroy()
+  io.destroy()
+})
+
 test('decode .aiff', (t) => {
   const audio = require('./test/fixtures/audio/sample.aiff', {
     with: { type: 'binary' }
