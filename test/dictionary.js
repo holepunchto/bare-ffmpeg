fc3952ee69627aff0a7a5d1aab55c8cc8714e7a1
Tony Gorez <gorez.tony@gmail.com>
2025-04-15 00:09:47 +0200

    test: wrap
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- test/dictionary.js
+++ test/dictionary.js
@@ -1,50 +1,52 @@
 const test = require('brittle')
 const ffmpeg = require('..')
 
-test('dictionary: basic set/get', (t) => {
-  const dict = new ffmpeg.Dictionary()
+test('dictionary', (t) => {
+  t.test('basic set/get', (t) => {
+    const dict = new ffmpeg.Dictionary()
 
-  dict.set('foo', 'bar')
-  t.is(dict.get('foo'), 'bar', 'retrieves value after set')
-})
+    dict.set('foo', 'bar')
+    t.is(dict.get('foo'), 'bar', 'retrieves value after set')
+  })
 
-test('dictionary: multiple entries', (t) => {
-  const dict = new ffmpeg.Dictionary()
+  t.test('multiple entries', (t) => {
+    const dict = new ffmpeg.Dictionary()
 
-  dict.set('a', '1')
-  dict.set('b', '2')
-  dict.set('c', '3')
+    dict.set('a', '1')
+    dict.set('b', '2')
+    dict.set('c', '3')
 
-  t.is(dict.get('a'), '1')
-  t.is(dict.get('b'), '2')
-  t.is(dict.get('c'), '3')
-})
+    t.is(dict.get('a'), '1')
+    t.is(dict.get('b'), '2')
+    t.is(dict.get('c'), '3')
+  })
 
-test('dictionary: overwrite existing key', (t) => {
-  const dict = new ffmpeg.Dictionary()
+  t.test('overwrite existing key', (t) => {
+    const dict = new ffmpeg.Dictionary()
 
-  dict.set('foo', 'bar')
-  dict.set('foo', 'baz')
+    dict.set('foo', 'bar')
+    dict.set('foo', 'baz')
 
-  t.is(dict.get('foo'), 'baz', 'last write wins')
-})
+    t.is(dict.get('foo'), 'baz', 'last write wins')
+  })
 
-test('dictionary: non-existent key returns null', (t) => {
-  const dict = new ffmpeg.Dictionary()
+  t.test('non-existent key returns null', (t) => {
+    const dict = new ffmpeg.Dictionary()
 
-  t.is(dict.get('missing'), null, 'getting unknown key yields null')
-})
+    t.is(dict.get('missing'), null, 'getting unknown key yields null')
+  })
 
-test('dictionary: handles empty string keys', (t) => {
-  const dict = new ffmpeg.Dictionary()
+  t.test('handles empty string keys', (t) => {
+    const dict = new ffmpeg.Dictionary()
 
-  dict.set('', 'empty')
-  t.is(dict.get(''), 'empty')
-})
+    dict.set('', 'empty')
+    t.is(dict.get(''), 'empty')
+  })
 
-test('dictionary: handles empty string values', (t) => {
-  const dict = new ffmpeg.Dictionary()
+  t.test('handles empty string values', (t) => {
+    const dict = new ffmpeg.Dictionary()
 
-  dict.set('key', '')
-  t.is(dict.get('key'), '')
+    dict.set('key', '')
+    t.is(dict.get('key'), '')
+  })
 })
