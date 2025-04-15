563b1f4820933e111ba7a948a8ac237a63b048e6
Tony Gorez <gorez.tony@gmail.com>
2025-04-15 10:39:36 +0200

    fix: handle param error in jsland
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- test/dictionary.js
+++ test/dictionary.js
@@ -1,52 +1,60 @@
 const test = require('brittle')
 const ffmpeg = require('..')
 
-test('dictionary', (t) => {
-  t.test('basic set/get', (t) => {
-    const dict = new ffmpeg.Dictionary()
+test('basic set/get', (t) => {
+  const dict = new ffmpeg.Dictionary()
 
-    dict.set('foo', 'bar')
-    t.is(dict.get('foo'), 'bar', 'retrieves value after set')
-  })
+  dict.set('foo', 'bar')
+  t.is(dict.get('foo'), 'bar', 'retrieves value after set')
+})
 
-  t.test('multiple entries', (t) => {
-    const dict = new ffmpeg.Dictionary()
+test('multiple entries', (t) => {
+  const dict = new ffmpeg.Dictionary()
 
-    dict.set('a', '1')
-    dict.set('b', '2')
-    dict.set('c', '3')
+  dict.set('a', '1')
+  dict.set('b', '2')
+  dict.set('c', '3')
 
-    t.is(dict.get('a'), '1')
-    t.is(dict.get('b'), '2')
-    t.is(dict.get('c'), '3')
-  })
+  t.is(dict.get('a'), '1')
+  t.is(dict.get('b'), '2')
+  t.is(dict.get('c'), '3')
+})
 
-  t.test('overwrite existing key', (t) => {
-    const dict = new ffmpeg.Dictionary()
+test('overwrite existing key', (t) => {
+  const dict = new ffmpeg.Dictionary()
 
-    dict.set('foo', 'bar')
-    dict.set('foo', 'baz')
+  dict.set('foo', 'bar')
+  dict.set('foo', 'baz')
 
-    t.is(dict.get('foo'), 'baz', 'last write wins')
-  })
+  t.is(dict.get('foo'), 'baz', 'last write wins')
+})
 
-  t.test('non-existent key returns null', (t) => {
-    const dict = new ffmpeg.Dictionary()
+test('non-existent key returns null', (t) => {
+  const dict = new ffmpeg.Dictionary()
 
-    t.is(dict.get('missing'), null, 'getting unknown key yields null')
-  })
+  t.is(dict.get('missing'), null, 'getting unknown key yields null')
+})
 
-  t.test('handles empty string keys', (t) => {
-    const dict = new ffmpeg.Dictionary()
+test('set throw when key is empty', (t) => {
+  const dict = new ffmpeg.Dictionary()
 
-    dict.set('', 'empty')
-    t.is(dict.get(''), 'empty')
-  })
+  t.exception.all(() => {
+    dict.set('', 'bar')
+  }, 'Key should be a non empty string')
+})
 
-  t.test('handles empty string values', (t) => {
-    const dict = new ffmpeg.Dictionary()
+test('set throw when value is empty', (t) => {
+  const dict = new ffmpeg.Dictionary()
 
-    dict.set('key', '')
-    t.is(dict.get('key'), '')
-  })
+  t.exception.all(() => {
+    dict.set('foo', '')
+  }, 'Value should be a non empty string')
+})
+
+test('get throw when key is empty', (t) => {
+  const dict = new ffmpeg.Dictionary()
+
+  t.exception.all(() => {
+    dict.get('')
+  }, 'Key should be a non empty string')
 })
