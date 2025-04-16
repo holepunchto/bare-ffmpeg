e78301a2e1d4f7e85f195410810703cae6252a9e
Tony Gorez <gorez.tony@gmail.com>
2025-04-16 10:18:59 +0200

    test: add input validation
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- test/input-format.js
+++ test/input-format.js
@@ -12,3 +12,15 @@ test('input format should throw if the name is wrong', (t) => {
     const inputFormat = new ffmpeg.InputFormat('foobar')
   }, `No input format found for name foobar`)
 })
+
+test('input format should throw if the name is empty', (t) => {
+  t.exception.all(() => {
+    const inputFormat = new ffmpeg.InputFormat('')
+  }, `Input format name should be a non empty string`)
+})
+
+test('input format should throw if the name is not a string', (t) => {
+  t.exception.all(() => {
+    const inputFormat = new ffmpeg.InputFormat(0)
+  }, `Input format name should be a non empty string`)
+})
