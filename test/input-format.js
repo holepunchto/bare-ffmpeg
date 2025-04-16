60a27027134da779ef246f284f3bd5e8341b78d0
Tony Gorez <gorez.tony@gmail.com>
2025-04-16 10:13:57 +0200

    test: add failure test
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- test/input-format.js
+++ test/input-format.js
@@ -6,3 +6,9 @@ test('input format should wrap an array buffer', (t) => {
 
   t.ok(inputFormat._handle instanceof ArrayBuffer)
 })
+
+test('input format should throw if the name is wrong', (t) => {
+  t.exception(() => {
+    const inputFormat = new ffmpeg.InputFormat('foobar')
+  }, `No input format found for name foobar`)
+})
