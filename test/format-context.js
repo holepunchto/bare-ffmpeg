da93ee46ba8f7e6007fb3f61db1bb9f885ba539d
Tony Gorez <gorez.tony@gmail.com>
2025-04-23 19:07:42 +0200

    test: use lavfi for linux
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- test/format-context.js
+++ test/format-context.js
@@ -1,6 +1,14 @@
 const test = require('brittle')
 const ffmpeg = require('..')
 
+let fallbackName
+let fallbackURL
+
+if (Bare.platform == 'linux') {
+  fallbackName = 'lavfi'
+  fallbackURL = 'testsrc=size=640x480:rate=30'
+}
+
 test('InputFormatContext should be instanciate with IOContext', (t) => {
   const image = require('./fixtures/image/sample.jpeg', {
     with: { type: 'binary' }
@@ -13,24 +21,32 @@ test('InputFormatContext should be instanciate with IOContext', (t) => {
 })
 
 test('InputFormatContext should be instanciate with InputFormat', (t) => {
-  const inputFormat = new ffmpeg.InputFormat()
+  const inputFormat = new ffmpeg.InputFormat(fallbackName)
   const options = new ffmpeg.Dictionary()
   options.set('framerate', '30')
   options.set('video_size', '1280x720')
   options.set('pixel_format', 'uyvy422')
 
-  const inputFormatContext = new ffmpeg.InputFormatContext(inputFormat, options)
+  const inputFormatContext = new ffmpeg.InputFormatContext(
+    inputFormat,
+    options,
+    fallbackURL
+  )
 
   t.ok(inputFormatContext)
 })
 
 test('InputFormatContext should be instanciate with InputFormat', (t) => {
-  const inputFormat = new ffmpeg.InputFormat()
+  const inputFormat = new ffmpeg.InputFormat(fallbackName)
   const options = new ffmpeg.Dictionary()
   options.set('framerate', '30')
   options.set('video_size', '1280x720')
   options.set('pixel_format', 'uyvy422')
-  const inputFormatContext = new ffmpeg.InputFormatContext(inputFormat, options)
+  const inputFormatContext = new ffmpeg.InputFormatContext(
+    inputFormat,
+    options,
+    fallbackURL
+  )
 
   const bestStream = inputFormatContext.getBestStream()
 
