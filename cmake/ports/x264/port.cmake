b1b31d6dd548bd16aa3defbdc5342c9b5fc6c5d7
Tony Gorez <gorez.tony@gmail.com>
2025-04-30 15:02:52 +0100

    feat(libx264): enable android
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- cmake/ports/x264/port.cmake
+++ cmake/ports/x264/port.cmake
@@ -21,12 +21,34 @@ if(IOS)
   list(APPEND args "--extra-ldflags=-arch ${CMAKE_SYSTEM_PROCESSOR} -isysroot ${CMAKE_OSX_SYSROOT}")
 endif()
 
+if(ANDROID)
+  message(STATUS "TEST -- ANDROID_NDK ${ANDROID_NDK}")
+  set(ndk_triple "${CMAKE_SYSTEM_PROCESSOR}-linux-android")
+
+  list(APPEND args --host=${ndk_triple})
+  list(APPEND args
+    "--extra-cflags=--target=${CMAKE_C_COMPILER_TARGET} -isysroot ${CMAKE_SYSROOT}"
+    "--extra-ldflags=--target=${CMAKE_C_COMPILER_TARGET} -isysroot ${CMAKE_SYSROOT}"
+  )
+  list(APPEND args --disable-asm)
+endif()
+
+set(env)
+if(ANDROID)
+  message(STATUS "TEST -- CMAKE_C_COMPILER ${CMAKE_C_COMPILER}")
+  if(CMAKE_C_COMPILER)
+
+    list(APPEND env "CC=${CMAKE_C_COMPILER}")
+  endif()
+endif()
+
 declare_port(
   "git:code.videolan.org/videolan/x264#stable"
   x264
   AUTOTOOLS
   BYPRODUCTS lib/libx264.a
   ARGS ${args}
+  ENV ${env}
 )
 
 add_library(x264 STATIC IMPORTED GLOBAL)
