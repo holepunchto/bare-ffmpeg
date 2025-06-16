d18276b4452255547b6ceed1cc493426db0698e2
Tony Gorez <gorez.tony@gmail.com>
2025-06-16 16:53:10 +0200

    refactor(packet): port to libjstl
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- binding.cc
+++ binding.cc
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <cstdint>
 #include <stddef.h>
 #include <stdint.h>
 #include <string.h>
@@ -940,14 +941,17 @@ bare_ffmpeg_image_get_line_size(
   );
 }
 
-static js_value_t *
-bare_ffmpeg_packet_init(js_env_t *env, js_callback_info_t *info) {
+static js_arraybuffer_t
+bare_ffmpeg_packet_init(
+  js_env_t *env,
+  js_receiver_t
+) {
   int err;
 
-  js_value_t *handle;
+  js_arraybuffer_t handle;
 
   bare_ffmpeg_packet_t *packet;
-  err = js_create_arraybuffer(env, sizeof(bare_ffmpeg_packet_t), (void **) &packet, &handle);
+  err = js_create_arraybuffer(env, packet, handle);
   assert(err == 0);
 
   packet->handle = av_packet_alloc();
@@ -955,29 +959,15 @@ bare_ffmpeg_packet_init(js_env_t *env, js_callback_info_t *info) {
   return handle;
 }
 
-static js_value_t *
-bare_ffmpeg_packet_init_from_buffer(js_env_t *env, js_callback_info_t *info) {
+static js_arraybuffer_t
+bare_ffmpeg_packet_init_from_buffer(
+  js_env_t *env,
+  js_receiver_t,
+  js_arraybuffer_span_t data,
+  int64_t offset,
+  int64_t len
+) {
   int err;
-
-  size_t argc = 3;
-  js_value_t *argv[3];
-
-  err = js_get_callback_info(env, info, &argc, argv, NULL, NULL);
-  assert(err == 0);
-
-  assert(argc == 3);
-
-  uint8_t *data;
-  err = js_get_arraybuffer_info(env, argv[0], (void **) &data, NULL);
-  assert(err == 0);
-
-  uint32_t offset;
-  err = js_get_value_uint32(env, argv[1], &offset);
-  assert(err == 0);
-
-  uint32_t len;
-  err = js_get_value_uint32(env, argv[2], &len);
-  assert(err == 0);
 
   AVPacket *pkt = av_packet_alloc();
   assert(pkt != NULL);
@@ -987,10 +977,9 @@ bare_ffmpeg_packet_init_from_buffer(js_env_t *env, js_callback_info_t *info) {
 
   memcpy(pkt->data, &data[offset], len);
 
-  js_value_t *handle;
-
+  js_arraybuffer_t handle;
   bare_ffmpeg_packet_t *packet;
-  err = js_create_arraybuffer(env, sizeof(bare_ffmpeg_packet_t), (void **) &packet, &handle);
+  err = js_create_arraybuffer(env, packet, handle);
   assert(err == 0);
 
   packet->handle = pkt;
@@ -998,91 +987,35 @@ bare_ffmpeg_packet_init_from_buffer(js_env_t *env, js_callback_info_t *info) {
   return handle;
 }
 
-static js_value_t *
-bare_ffmpeg_packet_destroy(js_env_t *env, js_callback_info_t *info) {
-  int err;
-
-  size_t argc = 1;
-  js_value_t *argv[1];
-
-  err = js_get_callback_info(env, info, &argc, argv, NULL, NULL);
-  assert(err == 0);
-
-  assert(argc == 1);
-
-  bare_ffmpeg_packet_t *packet;
-  err = js_get_arraybuffer_info(env, argv[0], (void **) &packet, NULL);
-  assert(err == 0);
-
+static void
+bare_ffmpeg_packet_unref(
+  js_env_t *env,
+  js_receiver_t,
+  js_arraybuffer_span_of_t<bare_ffmpeg_packet_t, 1> packet
+) {
   av_packet_unref(packet->handle);
-
-  return NULL;
 }
 
-static js_value_t *
-bare_ffmpeg_packet_unref(js_env_t *env, js_callback_info_t *info) {
-  int err;
-
-  size_t argc = 1;
-  js_value_t *argv[1];
-
-  err = js_get_callback_info(env, info, &argc, argv, NULL, NULL);
-  assert(err == 0);
-
-  assert(argc == 1);
-
-  bare_ffmpeg_packet_t *packet;
-  err = js_get_arraybuffer_info(env, argv[0], (void **) &packet, NULL);
-  assert(err == 0);
-
-  av_packet_unref(packet->handle);
-
-  return NULL;
+static int32_t
+bare_ffmpeg_packet_get_stream_index(
+  js_env_t *env,
+  js_receiver_t,
+  js_arraybuffer_span_of_t<bare_ffmpeg_packet_t, 1> packet
+) {
+  return packet->handle->stream_index;
 }
 
-static js_value_t *
-bare_ffmpeg_packet_get_stream_index(js_env_t *env, js_callback_info_t *info) {
+static js_arraybuffer_t
+bare_ffmpeg_packet_get_data(
+  js_env_t *env,
+  js_receiver_t,
+  js_arraybuffer_span_of_t<bare_ffmpeg_packet_t, 1> packet
+) {
   int err;
 
-  size_t argc = 1;
-  js_value_t *argv[1];
-
-  err = js_get_callback_info(env, info, &argc, argv, NULL, NULL);
-  assert(err == 0);
-
-  assert(argc == 1);
-
-  bare_ffmpeg_packet_t *packet;
-  err = js_get_arraybuffer_info(env, argv[0], (void **) &packet, NULL);
-  assert(err == 0);
-
-  js_value_t *result;
-  err = js_create_int32(env, packet->handle->stream_index, &result);
-  assert(err == 0);
-
-  return result;
-}
-
-static js_value_t *
-bare_ffmpeg_packet_get_data(js_env_t *env, js_callback_info_t *info) {
-  int err;
-
-  size_t argc = 1;
-  js_value_t *argv[1];
-
-  err = js_get_callback_info(env, info, &argc, argv, NULL, NULL);
-  assert(err == 0);
-
-  assert(argc == 1);
-
-  bare_ffmpeg_packet_t *packet;
-  err = js_get_arraybuffer_info(env, argv[0], (void **) &packet, NULL);
-  assert(err == 0);
-
-  js_value_t *handle;
-
+  js_arraybuffer_t handle;
   uint8_t *data;
-  err = js_create_arraybuffer(env, packet->handle->size, (void **) &data, &handle);
+  err = js_create_arraybuffer(env, packet->handle->size, data, handle);
   assert(err == 0);
 
   memcpy(data, packet->handle->data, packet->handle->size);
@@ -1412,6 +1345,12 @@ bare_ffmpeg_exports(js_env_t *env, js_value_t *exports) {
   V("initImage", bare_ffmpeg_image_init)
   V("fillImage", bare_ffmpeg_image_fill)
   V("getImageLineSize", bare_ffmpeg_image_get_line_size)
+
+  V("initPacket", bare_ffmpeg_packet_init)
+  V("initPacketFromBuffer", bare_ffmpeg_packet_init_from_buffer)
+  V("unrefPacket", bare_ffmpeg_packet_unref)
+  V("getPacketStreamIndex", bare_ffmpeg_packet_get_stream_index)
+  V("getPacketData", bare_ffmpeg_packet_get_data)
 #undef V
 
 #define V(name, fn) \
@@ -1422,13 +1361,6 @@ bare_ffmpeg_exports(js_env_t *env, js_value_t *exports) {
     err = js_set_named_property(env, exports, name, val); \
     assert(err == 0); \
   }
-
-  V("initPacket", bare_ffmpeg_packet_init)
-  V("initPacketFromBuffer", bare_ffmpeg_packet_init_from_buffer)
-  V("destroyPacket", bare_ffmpeg_packet_destroy)
-  V("unrefPacket", bare_ffmpeg_packet_unref)
-  V("getPacketStreamIndex", bare_ffmpeg_packet_get_stream_index)
-  V("getPacketData", bare_ffmpeg_packet_get_data)
 
   V("initScaler", bare_ffmpeg_scaler_init)
   V("destroyScaler", bare_ffmpeg_scaler_destroy)
