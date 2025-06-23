c69c3b3060e6801509db95183e7c748014e2d5d5
sethvincent <sethvincent@gmail.com>
2025-06-23 09:50:44 -0700

    tidy opus/port.cmake

--- cmake/ports/opus/port.cmake
+++ cmake/ports/opus/port.cmake
@@ -7,20 +7,13 @@ else()
 endif()
 
 set(args
-  -DCMAKE_POLICY_VERSION_MINIMUM=3.5
   -DBUILD_SHARED_LIBS=OFF
-  -DOPUS_BUILD_PROGRAMS=OFF
-  -DOPUS_BUILD_TESTING=OFF
-  -DOPUS_INSTALL_PKG_CONFIG_MODULE=ON
+  -DBUILD_TESTING=OFF
 )
 
-bare_arch(arch)
+message(arch="${arch}")
 
-if(arch MATCHES "arm64")
-  list(APPEND args
-    -DOPUS_USE_NEON=ON
-  )
-elseif(arch MATCHES "x64")
+if(arch MATCHES "x64")
   list(APPEND args
     -DOPUS_X86_MAY_HAVE_SSE4_1=ON
     -DOPUS_X86_MAY_HAVE_AVX2=ON
@@ -34,7 +27,7 @@ if(MSVC AND arch MATCHES "x64")
 endif()
 
 declare_port(
-  "github:xiph/opus#c0eb2ca"
+  "github:xiph/opus@v1.5.2"
   opus
   BYPRODUCTS lib/${lib}
   ARGS ${args}
