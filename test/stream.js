dcaaf42f008831a9c893c7782b3889a9fad0e74f
Tony Gorez <gorez.tony@gmail.com>
2025-07-10 16:13:54 +0200

    test(stream): add basic tests
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

---
+++ test/stream.js
@@ -0,0 +1,46 @@
+const test = require('brittle')
+const ffmpeg = require('..')
+
+const options = new ffmpeg.Dictionary()
+options.set('framerate', '30')
+options.set('video_size', '1280x720')
+options.set('pixel_format', 'uyvy422')
+using inputFormatContext = new ffmpeg.InputFormatContext(
+  new ffmpeg.InputFormat('lavfi'),
+  options,
+  'testsrc=size=640x480:rate=30'
+)
+const stream = inputFormatContext.getBestStream(ffmpeg.constants.mediaTypes.VIDEO)
+
+test('it should expose an ID getter', (t) => {
+  t.ok(typeof stream.id === 'number')
+})
+
+test('it should expose an ID setter', (t) => {
+  stream.id = 1
+  t.ok(stream.id === 1)
+})
+
+test('it should expose an index getter', (t) => {
+  t.ok(typeof stream.index === 'number')
+})
+
+test('it should expose a codec getter', (t) => {
+  t.ok(stream.codec instanceof ffmpeg.Codec)
+})
+
+test('it should expose a codec parameters getter', (t) => {
+  t.ok(stream.codecParameters instanceof ffmpeg.CodecParameters)
+})
+
+test('it should expose a timeBase getter', (t) => {
+  t.ok(stream.timeBase instanceof ffmpeg.Rational)
+})
+
+test('it should expose a timeBase setter', (t) => {
+  const newTimeBase = new ffmpeg.Rational(1, 30)
+  stream.timeBase = newTimeBase
+  t.ok(stream.timeBase.numerator === newTimeBase.numerator)
+  t.ok(stream.timeBase.denominator === newTimeBase.denominator)
+})
+
