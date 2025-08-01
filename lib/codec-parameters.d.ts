b3ee2981303507c1aa74800840f0819c44a2611e
Tony Gorez <gorez.tony@gmail.com>
2025-08-01 22:28:26 +0200

    chore: add ts type
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- lib/codec-parameters.d.ts
+++ lib/codec-parameters.d.ts
@@ -10,6 +10,7 @@ declare class FFmpegCodecParameters {
   readonly nbChannels: number
   readonly codecType: number
   readonly channelLayout: FFmpegChannelLayout
+  extraData: Buffer
 
   constructor(handle: ArrayBuffer)
 
