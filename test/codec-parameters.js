a4fd9111672048fcc5fc886481e219f1b7b46918
Tony Gorez <gorez.tony@gmail.com>
2025-08-01 22:27:50 +0200

    test: add setter case
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- test/codec-parameters.js
+++ test/codec-parameters.js
@@ -1,10 +1,22 @@
 const test = require('brittle')
 const ffmpeg = require('..')
 
-test('CodecParameters class should expose a extraData', (t) => {
+test('CodecParameters class should expose a extraData getter', (t) => {
   const codecParam = getCodecParameters()
 
   t.ok(codecParam.extraData instanceof Buffer) 
+})
+
+test('CodecParameters class should expose a extraData setter', (t) => {
+  const codecParam = getCodecParameters()
+
+  const buf = Buffer.from('test')
+  codecParam.extraData = buf
+
+  t.ok(codecParam.extraData[0] == 't'.charCodeAt(0))
+  t.ok(codecParam.extraData[1] == 'e'.charCodeAt(0))
+  t.ok(codecParam.extraData[2] == 's'.charCodeAt(0))
+  t.ok(codecParam.extraData[3] == 't'.charCodeAt(0))
 })
 
 // Helpers
@@ -14,7 +26,7 @@ function getCodecParameters() {
   options.set('framerate', '30')
   options.set('video_size', '1280x720')
   options.set('pixel_format', 'uyvy422')
-  const inputFormatContext = new ffmpeg.InputFormatContext(
+  using inputFormatContext = new ffmpeg.InputFormatContext(
     new ffmpeg.InputFormat('lavfi'),
     options,
     'testsrc=size=640x480:rate=30'
