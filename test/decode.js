d4e7b5644b85fa9a9f0838eda5818539e569523d
Darshan Sen <raisinten@gmail.com>
2025-08-07 20:05:44 +0530

    Add test
    Signed-off-by: Darshan Sen <raisinten@gmail.com>

--- test/decode.js
+++ test/decode.js
@@ -174,6 +174,7 @@ function decodeAudio(audio) {
       output.nbSamples
     )
     samples.fill(output)
+    t.ok(samples.data.some(i => i), 'Samples not Empty')
 
     while (resampler.flush(output) > 0) {
       buffers.push(Buffer.from(samples.data))
