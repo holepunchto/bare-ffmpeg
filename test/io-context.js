441317977f84136dc81f3844806614c54174b423
sethvincent <sethvincent@gmail.com>
2025-08-12 10:59:30 -0700

    simplify streaming io context tests, other small fixes

--- test/io-context.js
+++ test/io-context.js
@@ -3,7 +3,7 @@ const ffmpeg = require('..')
 
 const { mediaTypes } = ffmpeg.constants
 
-test('IOContext streaming webm without onseek', (t) => {
+test.solo('IOContext streaming webm with onread', (t) => {
   const data = require('./fixtures/video/sample.webm', {
     with: { type: 'binary' }
   })
@@ -23,43 +23,13 @@ test('IOContext streaming webm without onseek', (t) => {
     }
   })
 
-  using format = new ffmpeg.InputFormatContext(io)
-  using packet = new ffmpeg.Packet()
-
-  const streams = []
-  for (const stream of format.streams) {
-    streams[stream.index] = { stream, decoder: stream.decoder() }
-  }
-
-  let video = []
-  let audio = []
-
-  while (format.readFrame(packet)) {
-    const { stream, decoder } = streams[packet.streamIndex]
-    const mediaType = stream.codecParameters.type
-    decoder.sendPacket(packet)
-
-    while (decoder.receiveFrame(new ffmpeg.Frame())) {}
-
-    if (mediaType === mediaTypes.VIDEO) {
-      video.push(packet.data)
-    } else if (mediaType === mediaTypes.AUDIO) {
-      audio.push(packet.data)
-    }
-
-    packet.unref()
-  }
-
-  for (const { decoder } of streams) decoder?.destroy()
-
-  video = Buffer.concat(video)
-  audio = Buffer.concat(audio)
+  const { audio, video } = runStreams(io)
 
   t.is(video.length, 145416, 'video size matches')
   t.is(audio.length, 42419, 'audio size matches')
 })
 
-test('IOContext streaming mp4 with onseek', (t) => {
+test.solo('IOContext streaming mp4 with onseek', (t) => {
   const data = require('./fixtures/video/sample.mp4', {
     with: { type: 'binary' }
   })
@@ -82,6 +52,13 @@ test('IOContext streaming mp4 with onseek', (t) => {
     }
   })
 
+  const { audio, video } = runStreams(io)
+
+  t.is(video.length, 140102, 'video size matches')
+  t.is(audio.length, 34914, 'audio size matches')
+})
+
+function runStreams (io) {
   using format = new ffmpeg.InputFormatContext(io)
   using packet = new ffmpeg.Packet()
 
@@ -116,6 +93,5 @@ test('IOContext streaming mp4 with onseek', (t) => {
   video = Buffer.concat(video)
   audio = Buffer.concat(audio)
 
-  t.is(video.length, 140102, 'video size matches')
-  t.is(audio.length, 34914, 'audio size matches')
-})
+  return { video, audio }
+}
