89acee9e2804f542ca544cc9f0a07086361e635c
sethvincent <sethvincent@gmail.com>
2025-09-19 02:35:00 -0700

    use using in test/codec-config.js

--- test/codec-config.js
+++ test/codec-config.js
@@ -2,8 +2,8 @@ const test = require('brittle')
 const ffmpeg = require('..')
 
 test('getSupportedConfig for pixel formats', (t) => {
-  const codecCtx = new ffmpeg.CodecContext(ffmpeg.Codec.AV1.encoder)
-  using pixFormats = codecCtx.getSupportedConfig(
+  using codecCtx = new ffmpeg.CodecContext(ffmpeg.Codec.AV1.encoder)
+  const pixFormats = codecCtx.getSupportedConfig(
     ffmpeg.constants.codecConfig.PIX_FORMAT
   )
 
@@ -12,67 +12,57 @@ test('getSupportedConfig for pixel formats', (t) => {
 })
 
 test('getSupportedConfig for sample formats', (t) => {
-  const codecCtx = new ffmpeg.CodecContext(ffmpeg.Codec.AAC.encoder)
+  using codecCtx = new ffmpeg.CodecContext(ffmpeg.Codec.AAC.encoder)
   const sampleFormats = codecCtx.getSupportedConfig(
     ffmpeg.constants.codecConfig.SAMPLE_FORMAT
   )
 
   t.ok(sampleFormats instanceof Int32Array, 'returns Int32Array')
   t.ok(sampleFormats.length > 0, 'has sample formats')
-
-  codecCtx.destroy()
 })
 
 test('getSupportedConfig for sample rates', (t) => {
-  const codecCtx = new ffmpeg.CodecContext(ffmpeg.Codec.AAC.encoder)
+  using codecCtx = new ffmpeg.CodecContext(ffmpeg.Codec.AAC.encoder)
   const sampleRates = codecCtx.getSupportedConfig(
     ffmpeg.constants.codecConfig.SAMPLE_RATE
   )
 
   t.ok(sampleRates instanceof Int32Array, 'returns Int32Array')
   t.ok(sampleRates.length > 0, 'has sample rates')
-
-  codecCtx.destroy()
 })
 
 test('getSupportedConfig for color range', (t) => {
-  const codecCtx = new ffmpeg.CodecContext(ffmpeg.Codec.AV1.encoder)
+  using codecCtx = new ffmpeg.CodecContext(ffmpeg.Codec.AV1.encoder)
   const colorRanges = codecCtx.getSupportedConfig(
     ffmpeg.constants.codecConfig.COLOR_RANGE
   )
 
   t.ok(colorRanges instanceof Int32Array, 'returns Int32Array')
   t.ok(colorRanges.length > 0, 'has color ranges')
-
-  codecCtx.destroy()
 })
 
 test('getSupportedConfig for color space', (t) => {
-  const codecCtx = new ffmpeg.CodecContext(ffmpeg.Codec.AV1.encoder)
+  using codecCtx = new ffmpeg.CodecContext(ffmpeg.Codec.AV1.encoder)
   const colorSpaces = codecCtx.getSupportedConfig(
     ffmpeg.constants.codecConfig.COLOR_SPACE
   )
 
   t.ok(colorSpaces instanceof Int32Array, 'returns Int32Array')
   t.ok(colorSpaces.length > 0, 'has color spaces')
-
-  codecCtx.destroy()
 })
 
 test('getSupportedConfig for frame rates', (t) => {
-  const codecCtx = new ffmpeg.CodecContext(ffmpeg.Codec.AV1.encoder)
+  using codecCtx = new ffmpeg.CodecContext(ffmpeg.Codec.AV1.encoder)
   const frameRates = codecCtx.getSupportedConfig(
     ffmpeg.constants.codecConfig.FRAME_RATE
   )
   t.is(frameRates, null, 'all possible frame rates are accepted')
-  codecCtx.destroy()
 })
 
 test('getSupportedConfig for channel layouts', (t) => {
-  const codecCtx = new ffmpeg.CodecContext(ffmpeg.Codec.OPUS.encoder)
+  using codecCtx = new ffmpeg.CodecContext(ffmpeg.Codec.OPUS.encoder)
   const channelLayouts = codecCtx.getSupportedConfig(
     ffmpeg.constants.codecConfig.CHANNEL_LAYOUT
   )
   t.is(channelLayouts, null, 'all possible channel layouts are accepted')
-  codecCtx.destroy()
 })
