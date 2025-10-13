478f655f6b80e2e0f0793e36f3996f0c08f829e0
Tony Gorez <gorez.tony@gmail.com>
2025-10-13 19:23:07 +0200

    nofix: try av1
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- test/codec-hw-config.js
+++ test/codec-hw-config.js
@@ -2,7 +2,7 @@ const test = require('brittle')
 const ffmpeg = require('..')
 
 test.skip('CodecHWConfig should expose a methods getter', (t) => {
-  const hwConfig = ffmpeg.Codec.MJPEG.decoder.getHardwareConfigAt(0)
+  const hwConfig = ffmpeg.Codec.AV1.decoder.getHardwareConfigAt(0)
 
   t.is(typeof hwConfig.methods, 'number')
 })
--- test/encoder-decoder.js
+++ test/encoder-decoder.js
@@ -4,27 +4,27 @@ const ffmpeg = require('..')
 // Encoder
 
 test('Encoder.getHardwareConfigAt should return null when config is not available', (t) => {
-  const { encoder } = ffmpeg.Codec.MJPEG
+  const { encoder } = ffmpeg.Codec.AV1
 
   t.is(encoder.getHardwareConfigAt(0), null)
 })
 
 test.skip('Encoder should expose getHardwareConfigAt method', (t) => {
-  const { encoder } = ffmpeg.Codec.MJPEG
+  const { encoder } = ffmpeg.Codec.AV1
 
-  t.ok(encoder.getHardwareConfigAt(1) instanceof ffmpeg.CodecHWConfig)
+  t.ok(encoder.getHardwareConfigAt(0) instanceof ffmpeg.CodecHWConfig)
 })
 
 // Decoder
 
 test('Decoder.getHardwareConfigAt should return null when config is not available', (t) => {
-  const { decoder } = ffmpeg.Codec.MJPEG
+  const { decoder } = ffmpeg.Codec.AV1
 
   t.is(decoder.getHardwareConfigAt(0), null)
 })
 
 test.skip('Decoder should expose getHardwareConfigAt method', (t) => {
-  const { decoder } = ffmpeg.Codec.MJPEG
+  const { decoder } = ffmpeg.Codec.AV1
 
-  t.ok(decoder.getHardwareConfigAt(1) instanceof ffmpeg.CodecHWConfig)
+  t.ok(decoder.getHardwareConfigAt(0) instanceof ffmpeg.CodecHWConfig)
 })
