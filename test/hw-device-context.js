7c8770f8fca972b9084c1c2e10eb137b2a279f5a
Tony Gorez <gorez.tony@gmail.com>
2025-11-13 15:06:37 +0100

    test(hw-device-context): add device test and wrap into macos branch
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- test/hw-device-context.js
+++ test/hw-device-context.js
@@ -1,21 +1,32 @@
+const os = require('bare-os')
 const test = require('brittle')
 const ffmpeg = require('..')
 
-test('HWDeviceContext should instantiate with device type', (t) => {
-  const hwDevice = new ffmpeg.HWDeviceContext(ffmpeg.constants.hwDeviceTypes.VIDEOTOOLBOX)
-  t.ok(hwDevice)
-})
-
-test('HWDeviceContext should expose a destroy method', (t) => {
-  const hwDevice = new ffmpeg.HWDeviceContext(ffmpeg.constants.hwDeviceTypes.VIDEOTOOLBOX)
-
-  t.execution(() => {
-    hwDevice.destroy()
+if (os.hostname() == 'darwin') {
+  test('HWDeviceContext should instantiate with device type', (t) => {
+    const hwDevice = new ffmpeg.HWDeviceContext(ffmpeg.constants.hwDeviceTypes.VIDEOTOOLBOX)
+    t.ok(hwDevice)
   })
-})
 
-test('HWDeviceContext should work with using declaration', (t) => {
-  t.execution(() => {
-    using _hwDevice = new ffmpeg.HWDeviceContext(ffmpeg.constants.hwDeviceTypes.VIDEOTOOLBOX)
+  test('HWDeviceContext should expose a destroy method', (t) => {
+    const hwDevice = new ffmpeg.HWDeviceContext(ffmpeg.constants.hwDeviceTypes.VIDEOTOOLBOX)
+
+    t.execution(() => {
+      hwDevice.destroy()
+    })
   })
-})
+
+  test('HWDeviceContext should work with using declaration', (t) => {
+    t.execution(() => {
+      using _hwDevice = new ffmpeg.HWDeviceContext(ffmpeg.constants.hwDeviceTypes.VIDEOTOOLBOX)
+    })
+  })
+
+  test('HWDeviceContext should work without device parameter (real-world VideoToolbox usage)', (t) => {
+    t.execution(() => {
+      // VideoToolbox on macOS doesn't require a device string (uses available hardware automatically)
+      const hwDevice = new ffmpeg.HWDeviceContext(ffmpeg.constants.hwDeviceTypes.VIDEOTOOLBOX, null)
+      hwDevice.destroy()
+    })
+  })
+}
