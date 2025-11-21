cb3cc8735b66edde0364ac5002af181fa581bc0e
Tony Gorez <gorez.tony@gmail.com>
2025-11-21 10:32:10 +0100

    test: sync with macos

--- test/hw-device-context.js
+++ test/hw-device-context.js
@@ -4,7 +4,7 @@ const ffmpeg = require('..')
 
 if (os.platform() === 'darwin') {
   test('HWDeviceContext should instantiate with device type', (t) => {
-    const hwDevice = new ffmpeg.HWDeviceContext(ffmpeg.constants.hwDeviceTypes.VIDEOTOOLBOX)
+    using hwDevice = new ffmpeg.HWDeviceContext(ffmpeg.constants.hwDeviceTypes.VIDEOTOOLBOX)
     t.ok(hwDevice)
   })
 
@@ -15,17 +15,25 @@ if (os.platform() === 'darwin') {
       hwDevice.destroy()
     })
   })
-
-  test('HWDeviceContext should work with using declaration', (t) => {
-    t.execution(() => {
-      using _hwDevice = new ffmpeg.HWDeviceContext(ffmpeg.constants.hwDeviceTypes.VIDEOTOOLBOX)
-    })
-  })
 }
 
 if (os.platform() === 'linux') {
   test('HWDeviceContext should instantiate with device type', (t) => {
-    using hwDevice = new ffmpeg.HWDeviceContext(ffmpeg.constants.hwDeviceTypes.VAAPI, '/dev/dri/renderD128')
+    using hwDevice = new ffmpeg.HWDeviceContext(
+      ffmpeg.constants.hwDeviceTypes.VAAPI,
+      '/dev/dri/renderD128'
+    )
     t.ok(hwDevice)
+  })
+
+  test('HWDeviceContext should expose a destroy method', (t) => {
+    const hwDevice = new ffmpeg.HWDeviceContext(
+      ffmpeg.constants.hwDeviceTypes.VAAPI,
+      '/dev/dri/renderD128'
+    )
+
+    t.execution(() => {
+      hwDevice.destroy()
+    })
   })
 }
