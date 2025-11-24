b11f3c449ba5d976ed592ee7f71fffd7f9e352a8
Tony Gorez <gorez.tony@gmail.com>
2025-11-24 16:16:37 +0100

    feat(codec-context): add hw device getter and getter
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- test/hw-device-context.js
+++ test/hw-device-context.js
@@ -42,3 +42,37 @@ test('HWDeviceContext should expose a destroy method (linux)', linuxFilter, (t) 
     hwDevice.destroy()
   })
 })
+
+// CodecContext.hwDeviceCtx tests
+
+test('CodecContext.hwDeviceCtx should be settable (darwin)', darwinFilter, (t) => {
+  using hwDevice = new ffmpeg.HWDeviceContext(ffmpeg.constants.hwDeviceTypes.VIDEOTOOLBOX)
+  using codecContext = new ffmpeg.CodecContext(ffmpeg.Codec.AV1.decoder)
+
+  t.execution(() => {
+    codecContext.hwDeviceCtx = hwDevice
+  })
+})
+
+test('CodecContext.hwDeviceCtx should be gettable after setting (darwin)', darwinFilter, (t) => {
+  using hwDevice = new ffmpeg.HWDeviceContext(ffmpeg.constants.hwDeviceTypes.VIDEOTOOLBOX)
+  using codecContext = new ffmpeg.CodecContext(ffmpeg.Codec.AV1.decoder)
+
+  codecContext.hwDeviceCtx = hwDevice
+
+  const retrieved = codecContext.hwDeviceCtx
+  t.ok(retrieved)
+  t.ok(retrieved instanceof ffmpeg.HWDeviceContext)
+})
+
+test('CodecContext.hwDeviceCtx should return null when not set (darwin)', darwinFilter, (t) => {
+  using codecContext = new ffmpeg.CodecContext(ffmpeg.Codec.AV1.decoder)
+
+  const hwDeviceCtx = codecContext.hwDeviceCtx
+  t.is(hwDeviceCtx, null)
+})
+
+test('HWDeviceContext.from should return null for null handle', (t) => {
+  const result = ffmpeg.HWDeviceContext.from(null)
+  t.is(result, null)
+})
