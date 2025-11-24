7b300af8d1627e9ec54ac49d712c823823a9af03
Tony Gorez <gorez.tony@gmail.com>
2025-11-24 10:36:58 +0100

    test: use filters
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- test/hw-device-context.js
+++ test/hw-device-context.js
@@ -3,42 +3,42 @@ const test = require('brittle')
 const ffmpeg = require('..')
 const process = require('bare-process')
 
-if (os.platform() === 'darwin') {
-  test('HWDeviceContext should instantiate with device type', (t) => {
-    using hwDevice = new ffmpeg.HWDeviceContext(ffmpeg.constants.hwDeviceTypes.VIDEOTOOLBOX)
-    t.ok(hwDevice)
+// Darwin
+
+const darwinFilter = { skip: os.platform() !== 'darwin' }
+
+test('HWDeviceContext should instantiate with device type (darwin)', darwinFilter, (t) => {
+  using hwDevice = new ffmpeg.HWDeviceContext(ffmpeg.constants.hwDeviceTypes.VIDEOTOOLBOX)
+  t.ok(hwDevice)
+})
+
+test('HWDeviceContext should expose a destroy method (darwin)', darwinFilter, (t) => {
+  const hwDevice = new ffmpeg.HWDeviceContext(ffmpeg.constants.hwDeviceTypes.VIDEOTOOLBOX)
+
+  t.execution(() => {
+    hwDevice.destroy()
   })
+})
 
-  test('HWDeviceContext should expose a destroy method', (t) => {
-    const hwDevice = new ffmpeg.HWDeviceContext(ffmpeg.constants.hwDeviceTypes.VIDEOTOOLBOX)
+// Linux
 
-    t.execution(() => {
-      hwDevice.destroy()
-    })
+const linuxFilter = { skip: process.env.CI || os.platform() !== 'linux' }
+
+test('HWDeviceContext should instantiate with device type (linux)', linuxFilter, (t) => {
+  using hwDevice = new ffmpeg.HWDeviceContext(
+    ffmpeg.constants.hwDeviceTypes.VAAPI,
+    '/dev/dri/renderD128'
+  )
+  t.ok(hwDevice)
+})
+
+test('HWDeviceContext should expose a destroy method (linux)', linuxFilter, (t) => {
+  const hwDevice = new ffmpeg.HWDeviceContext(
+    ffmpeg.constants.hwDeviceTypes.VAAPI,
+    '/dev/dri/renderD128'
+  )
+
+  t.execution(() => {
+    hwDevice.destroy()
   })
-}
-
-if (os.platform() === 'linux') {
-  if (process.env.CI) {
-    return
-  }
-
-  test('HWDeviceContext should instantiate with device type', (t) => {
-    using hwDevice = new ffmpeg.HWDeviceContext(
-      ffmpeg.constants.hwDeviceTypes.VAAPI,
-      '/dev/dri/renderD128'
-    )
-    t.ok(hwDevice)
-  })
-
-  test('HWDeviceContext should expose a destroy method', (t) => {
-    const hwDevice = new ffmpeg.HWDeviceContext(
-      ffmpeg.constants.hwDeviceTypes.VAAPI,
-      '/dev/dri/renderD128'
-    )
-
-    t.execution(() => {
-      hwDevice.destroy()
-    })
-  })
-}
+})
