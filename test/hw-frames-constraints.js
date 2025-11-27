27c0c79a2d198e586ce22b021b2c2453d0ba70e4
Tony Gorez <gorez.tony@gmail.com>
2025-11-27 15:02:12 +0100

    chore: Lint
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- test/hw-frames-constraints.js
+++ test/hw-frames-constraints.js
@@ -20,14 +20,22 @@ test.hook('setup', { skip: darwinFilter }, () => {
   constraints = hwFrames.getConstraints()
 })
 
-test('HWFramesConstraints should expose validSwFormats getter (darwin)', { skip: darwinFilter }, (t) => {
-  t.ok(Array.isArray(constraints.validSwFormats))
-})
+test(
+  'HWFramesConstraints should expose validSwFormats getter (darwin)',
+  { skip: darwinFilter },
+  (t) => {
+    t.ok(Array.isArray(constraints.validSwFormats))
+  }
+)
 
-test('HWFramesConstraints should expose validHwFormats getter (darwin)', { skip: darwinFilter }, (t) => {
-  t.ok(Array.isArray(constraints.validHwFormats))
-  t.ok(constraints.validHwFormats.length > 0)
-})
+test(
+  'HWFramesConstraints should expose validHwFormats getter (darwin)',
+  { skip: darwinFilter },
+  (t) => {
+    t.ok(Array.isArray(constraints.validHwFormats))
+    t.ok(constraints.validHwFormats.length > 0)
+  }
+)
 
 test('HWFramesConstraints should expose minWidth getter (darwin)', { skip: darwinFilter }, (t) => {
   t.is(typeof constraints.minWidth, 'number')
@@ -45,15 +53,23 @@ test('HWFramesConstraints should expose maxHeight getter (darwin)', { skip: darw
   t.is(typeof constraints.maxHeight, 'number')
 })
 
-test('HWFramesConstraints width constraints should be sensible (darwin)', { skip: darwinFilter }, (t) => {
-  t.ok(constraints.minWidth >= 0)
-  t.ok(constraints.maxWidth >= constraints.minWidth)
-})
+test(
+  'HWFramesConstraints width constraints should be sensible (darwin)',
+  { skip: darwinFilter },
+  (t) => {
+    t.ok(constraints.minWidth >= 0)
+    t.ok(constraints.maxWidth >= constraints.minWidth)
+  }
+)
 
-test('HWFramesConstraints height constraints should be sensible (darwin)', { skip: darwinFilter }, (t) => {
-  t.ok(constraints.minHeight >= 0)
-  t.ok(constraints.maxHeight >= constraints.minHeight)
-})
+test(
+  'HWFramesConstraints height constraints should be sensible (darwin)',
+  { skip: darwinFilter },
+  (t) => {
+    t.ok(constraints.minHeight >= 0)
+    t.ok(constraints.maxHeight >= constraints.minHeight)
+  }
+)
 
 test('HWFramesConstraints should have destroy method (darwin)', { skip: darwinFilter }, (t) => {
   t.ok(typeof constraints.destroy === 'function')
