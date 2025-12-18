9fbcf7c3b8d50d8af42eb661712ef9277f19ed6f
Tony Gorez <gorez.tony@gmail.com>
2025-12-18 12:06:50 +0100

    test(encoder,decoder): remove asserts on internals
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- test/decoder.js
+++ test/decoder.js
@@ -4,29 +4,21 @@ const ffmpeg = require('..')
 test('Decoder.byName() for vp8', (t) => {
   const decoder = ffmpeg.Decoder.byName('vp8')
   t.ok(decoder, 'vp8 decoder found')
-  t.ok(decoder._handle, 'decoder has handle')
-  t.is(decoder._codec.name, 'vp8', 'codec name is vp8')
 })
 
 test('Decoder.byName() for vp9', (t) => {
   const decoder = ffmpeg.Decoder.byName('vp9')
   t.ok(decoder, 'vp9 decoder found')
-  t.ok(decoder._handle, 'decoder has handle')
-  t.is(decoder._codec.name, 'vp9', 'codec name is vp9')
 })
 
 test('Decoder.byName() for h264', (t) => {
   const decoder = ffmpeg.Decoder.byName('h264')
   t.ok(decoder, 'h264 decoder found')
-  t.ok(decoder._handle, 'decoder has handle')
-  t.is(decoder._codec.name, 'h264', 'codec name is h264')
 })
 
 test('Decoder.byName() for libdav1d (AV1)', (t) => {
   const decoder = ffmpeg.Decoder.byName('libdav1d')
   t.ok(decoder, 'libdav1d decoder found')
-  t.ok(decoder._handle, 'decoder has handle')
-  t.is(decoder._codec.name, 'libdav1d', 'codec name is libdav1d')
 })
 
 test('Decoder.byName() throws for non-existent decoder', (t) => {
--- test/encoder.js
+++ test/encoder.js
@@ -4,29 +4,21 @@ const ffmpeg = require('..')
 test('Encoder.byName() for libvpx (VP8)', (t) => {
   const encoder = ffmpeg.Encoder.byName('libvpx')
   t.ok(encoder, 'libvpx encoder found')
-  t.ok(encoder._handle, 'encoder has handle')
-  t.is(encoder._codec.name, 'libvpx', 'codec name is libvpx')
 })
 
 test('Encoder.byName() for libvpx-vp9 (VP9)', (t) => {
   const encoder = ffmpeg.Encoder.byName('libvpx-vp9')
   t.ok(encoder, 'libvpx-vp9 encoder found')
-  t.ok(encoder._handle, 'encoder has handle')
-  t.is(encoder._codec.name, 'libvpx-vp9', 'codec name is libvpx-vp9')
 })
 
 test('Encoder.byName() for libsvtav1 (AV1)', (t) => {
   const encoder = ffmpeg.Encoder.byName('libsvtav1')
   t.ok(encoder, 'libsvtav1 encoder found')
-  t.ok(encoder._handle, 'encoder has handle')
-  t.is(encoder._codec.name, 'libsvtav1', 'codec name is libsvtav1')
 })
 
 test('Encoder.byName() for libopus (OPUS)', (t) => {
   const encoder = ffmpeg.Encoder.byName('libopus')
   t.ok(encoder, 'libopus encoder found')
-  t.ok(encoder._handle, 'encoder has handle')
-  t.is(encoder._codec.name, 'libopus', 'codec name is libopus')
 })
 
 test('Encoder.byName() throws for non-existent encoder', (t) => {
