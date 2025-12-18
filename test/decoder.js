151653b874c6359e50b3ea1be2bedfb6a9ce1391
Tony Gorez <gorez.tony@gmail.com>
2025-12-18 13:37:40 +0100

    test(encoder,decoder): add test by id
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- test/decoder.js
+++ test/decoder.js
@@ -6,8 +6,18 @@ test('Decoder with string name for vp8', (t) => {
   t.ok(decoder, 'vp8 decoder found')
 })
 
+test('Decoder with VP8 Codec', (t) => {
+  const decoder = new ffmpeg.Decoder(ffmpeg.Codec.VP8)
+  t.ok(decoder, 'vp8 decoder found')
+})
+
 test('Decoder with string name for vp9', (t) => {
   const decoder = new ffmpeg.Decoder('vp9')
+  t.ok(decoder, 'vp9 decoder found')
+})
+
+test('Decoder with VP9 Codec', (t) => {
+  const decoder = new ffmpeg.Decoder(ffmpeg.Codec.VP9)
   t.ok(decoder, 'vp9 decoder found')
 })
 
@@ -16,9 +26,19 @@ test('Decoder with string name for h264', (t) => {
   t.ok(decoder, 'h264 decoder found')
 })
 
+test('Decoder with H264 Codec', (t) => {
+  const decoder = new ffmpeg.Decoder(ffmpeg.Codec.H264)
+  t.ok(decoder, 'h264 decoder found')
+})
+
 test('Decoder with string name for libdav1d (AV1)', (t) => {
   const decoder = new ffmpeg.Decoder('libdav1d')
   t.ok(decoder, 'libdav1d decoder found')
+})
+
+test('Decoder with AV1 Codec', (t) => {
+  const decoder = new ffmpeg.Decoder(ffmpeg.Codec.AV1)
+  t.ok(decoder, 'av1 decoder found')
 })
 
 test('Decoder throws for non-existent decoder', (t) => {
--- test/encoder.js
+++ test/encoder.js
@@ -6,9 +6,19 @@ test('Encoder with string name for libvpx (VP8)', (t) => {
   t.ok(encoder, 'libvpx encoder found')
 })
 
+test('Encoder with VP8 Codec', (t) => {
+  const encoder = new ffmpeg.Encoder(ffmpeg.Codec.VP8)
+  t.ok(encoder, 'libvpx encoder found')
+})
+
 test('Encoder with string name for libvpx-vp9 (VP9)', (t) => {
   const encoder = new ffmpeg.Encoder('libvpx-vp9')
   t.ok(encoder, 'libvpx-vp9 encoder found')
+})
+
+test('Encoder with VP9 Codec', (t) => {
+  const encoder = new ffmpeg.Encoder(ffmpeg.Codec.VP9)
+  t.ok(encoder, 'vp9 encoder found')
 })
 
 test('Encoder with string name for libsvtav1 (AV1)', (t) => {
@@ -16,9 +26,19 @@ test('Encoder with string name for libsvtav1 (AV1)', (t) => {
   t.ok(encoder, 'libsvtav1 encoder found')
 })
 
+test('Encoder with AV1 Codec', (t) => {
+  const encoder = new ffmpeg.Encoder(ffmpeg.Codec.AV1)
+  t.ok(encoder, 'av1 encoder found')
+})
+
 test('Encoder with string name for libopus (OPUS)', (t) => {
   const encoder = new ffmpeg.Encoder('libopus')
   t.ok(encoder, 'libopus encoder found')
+})
+
+test('Encoder with OPUS Codec', (t) => {
+  const encoder = new ffmpeg.Encoder(ffmpeg.Codec.OPUS)
+  t.ok(encoder, 'opus encoder found')
 })
 
 test('Encoder throws for non-existent encoder', (t) => {
